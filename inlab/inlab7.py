c9be5c5ef2f9eb1432b4b5d39bf61bcfb2999e7f
digi0ps <sriru1998@gmail.com>
2016-10-24 14:09:07 +0530

    Updated inlab7.py with operations.

--- inlab/inlab7.py
+++ inlab/inlab7.py
@@ -1,43 +1,36 @@
-#Inlab 7 ~ digi0ps
-# Operation A
 def readfile():
 	with open("ffcs.csv", "r") as file:
 		for record in file:
 			print(record,end="");
-
 readfile();
-
-#Operation B
-with open("ffcs.csv", "a") as file:
-	new1 = [str(input()) for i in range(8)];
-	file.write(','.join(new1)+"\n");
-readfile();
-
-#Operation C
-with open("ffcs.csv", "r+") as file:
-	new2 = [str(input()) for i in range(8)];
-	flag = 1;
-	for record in file:
-		if new2[0] == record.split(",")[0]:
-			flag = 0;
-			break;
-	if flag:
-		file.write("\n"+",".join(new2));
+def opB():
+	with open("ffcs.csv", "a") as file:
+		new1 = [str(input()) for i in range(8)];
+		file.write(','.join(new1)+"\n");
 	readfile();
-
-#Operation D
-with open("ffcs.csv", "r") as file:
-	malplayers = 0;
-	for record in file:
-		if "Kerala" in record:
-			malplayers+=1;
-	print(malplayers);
-
-#Operation E
-with open("ffcs.csv", "r") as file:
-	for record in file:
-		if "Tamilnadu" in record and "A+" in record:
-			fields = record.split(",");
-			print(fields[1], fields[4]);
-
+def opC():
+	with open("ffcs.csv", "r+") as file:
+		new2 = [str(input()) for i in range(8)];
+		flag = 1;
+		for record in file:
+			if new2[0] == record.split(",")[0]:
+				flag = 0;
+				break;
+		if flag:
+			file.write("\n"+",".join(new2));
+		readfile();
+def opD():
+	with open("ffcs.csv", "r") as file:
+		malplayers = 0;
+		for record in file:
+			if "Kerala" in record:
+				malplayers+=1;
+		print(malplayers);
+def opE():
+	with open("ffcs.csv", "r") as file:
+		for record in file:
+			if "Tamilnadu" in record and "A+" in record:
+				fields = record.split(",");
+				print(fields[1], fields[4]);
+opB(); opC(); opD(); opE();
 #www.github.com/digi0ps
